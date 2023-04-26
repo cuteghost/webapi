b57329fcb0085c4e8fecb7f66c634d93e6f3aa79
Dzevad.Alibegovic <dzevad.alibegovic@edu.fit.ba>
2023-04-26 18:32:32 +0200

    feat(dentalapp) 1. Updated Dockerfile 2. Created Dockercompose 3. Setup docker-compose for https testing

--- obj/Debug/net7.0/webapi.AssemblyInfo.cs
+++ obj/Debug/net7.0/webapi.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("nekakavSecret")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
